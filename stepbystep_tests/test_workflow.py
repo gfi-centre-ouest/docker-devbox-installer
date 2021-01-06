cc81ae5882f9710b014e1193c61024d78b84d7d4
Rémi Alvergnat <remi.alvergnat@gfi.world>
2021-01-07 00:04:23 +0100

    feat(context): add run context for step and workflow

--- stepbystep_tests/test_workflow.py
+++ stepbystep_tests/test_workflow.py
@@ -1,4 +1,5 @@
-from stepbystep import Workflow, StepFactory, StepModel, Step, WorkflowModel
+from stepbystep import Workflow, StepFactory, StepModel, Step, WorkflowModel, WorkflowRunContext, BaseRunContext
+from stepbystep.steps import NoopStep
 
 
 def test_dummy_workflow():
@@ -19,11 +20,11 @@ def test_dummy_workflow():
             dummy2_called_config = self.model.config
 
     class DummyStepFactory(StepFactory):
-        def build_step(self, model: StepModel, workflow: Workflow) -> Step:
+        def build_step(self, model: StepModel, workflow: Workflow, context: WorkflowRunContext) -> Step:
             if model.name == 'dummy1':
-                return Dummy1Step(model, workflow)
+                return Dummy1Step(model, workflow, context)
             if model.name == 'dummy2':
-                return Dummy2Step(model, workflow)
+                return Dummy2Step(model, workflow, context)
             return None
 
     step_factory = DummyStepFactory()
@@ -34,3 +35,102 @@ def test_dummy_workflow():
 
     assert dummy1_called_config == {'foo': 'bar1'}
     assert dummy2_called_config == {'foo': 'bar2'}
+
+
+class TestRunContext:
+    def test_base_run_context(self):
+        context = BaseRunContext()
+
+        context['test'] = 'abc'
+        assert context['test'] == 'abc'
+        assert len(context) == 1
+        for k, v in context.items():
+            assert k == 'test'
+            assert v == 'abc'
+        del context['test']
+        assert len(context) == 0
+        assert context.get('test', 'default') == 'default'
+
+    def test_workflow_run_context_step_not_found(self):
+        context = WorkflowRunContext()
+
+        assert context.step("not found") is None
+        assert context.step(3) is None
+        assert context.step(StepModel("not found")) is None
+        assert context.step(NoopStep(StepModel("not found"), None, None)) is None
+
+    def test_workflow_run_context_step_found(self):
+        context = WorkflowRunContext()
+
+        step_model = StepModel("noop")
+        step = NoopStep(step_model, None, context)
+        context.register_step(step)["foo"] = "bar"
+
+        assert context.step("noop") == {"foo": "bar"}
+        assert context.step(0) == {"foo": "bar"}
+        assert context.step(step_model) == {"foo": "bar"}
+        assert context.step(step) == {"foo": "bar"}
+
+        assert step.context == {"foo": "bar"}
+        assert step.workflow_context is context
+
+    def test_workflow_run_context_steps_found(self):
+        context = WorkflowRunContext()
+
+        step_model = StepModel("noop")
+        step = NoopStep(step_model, None, context)
+        context.register_step(step)["foo"] = "bar"
+
+        assert context.steps("noop") == [{"foo": "bar"}]
+        assert context.steps(0) == [{"foo": "bar"}]
+        assert context.steps(step_model) == [{"foo": "bar"}]
+        assert context.steps(step) == [{"foo": "bar"}]
+
+        assert step.context == {"foo": "bar"}
+        assert step.workflow_context is context
+
+    def test_workflow_run_context_step_found_default_solver(self):
+        context = WorkflowRunContext()
+
+        step_model = StepModel("noop")
+        step = NoopStep(step_model, None, context)
+        context.register_step(step)["foo"] = "bar"
+
+        step_model2 = StepModel("noop", {"config": "2"})
+        step2 = NoopStep(step_model2, None, context)
+        context.register_step(step2)["foo"] = "bar2"
+
+        assert context.step("noop") == {"foo": "bar2"}
+        assert context.step(0) == {"foo": "bar"}
+        assert context.step(1) == {"foo": "bar2"}
+        assert context.step(step_model) == {"foo": "bar"}
+        assert context.step(step_model2) == {"foo": "bar2"}
+        assert context.step(step) == {"foo": "bar"}
+        assert context.step(step2) == {"foo": "bar2"}
+
+        assert step.context == {"foo": "bar"}
+        assert step2.context == {"foo": "bar2"}
+
+    def test_workflow_run_context_step_found_custom_solver(self):
+        context = WorkflowRunContext()
+
+        step_model = StepModel("noop")
+        step = NoopStep(step_model, None, context)
+        context.register_step(step)["foo"] = "bar"
+
+        step_model2 = StepModel("noop", {"config": "2"})
+        step2 = NoopStep(step_model2, None, context)
+        context.register_step(step2)["foo"] = "bar2"
+
+        solver = lambda contexts: contexts[0] if contexts else None
+
+        assert context.step("noop", solver) == {"foo": "bar"}
+        assert context.step(0, solver) == {"foo": "bar"}
+        assert context.step(1, solver) == {"foo": "bar2"}
+        assert context.step(step_model, solver) == {"foo": "bar"}
+        assert context.step(step_model2, solver) == {"foo": "bar2"}
+        assert context.step(step, solver) == {"foo": "bar"}
+        assert context.step(step2, solver) == {"foo": "bar2"}
+
+        assert step.context == {"foo": "bar"}
+        assert step2.context == {"foo": "bar2"}
